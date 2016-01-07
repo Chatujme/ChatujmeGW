111ffbf7de0c9d13e25861ad7cd4345c411ac2dd
LuRy <lury@lury.cz>
2016-01-07 03:55:12 +0100

    add setup.py for py2exe

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding=utf8  
+from distutils.core import setup
+import py2exe
+
+setup(
+  name = 'ChatujmeGW IRC Gateway',
+  version = '1.3',
+  description = 'IRC brana pro pristup k chatu Chatujme.cz',
+  author = 'LuRy',
+  author_email = 'lury@lury.cz',
+  maintainer='LuRy',
+  maintainer_email='lury@llury.cz',
+  license = 'MIT',
+  platforms = ["Windows"],
+  url = 'http://lury.cz',
+  zipfile = "shared.lib",
+  #packages = ['....', '....'],
+  console = [{
+      'script': 'chatujmegw.py'
+      #,"icon_resources": [(0, "")]
+  }],
+  options = {
+      "py2exe": {
+          "optimize": 2,
+          "compressed" : True,
+          'bundle_files': 1
+          
+      } 
+    }
+)
