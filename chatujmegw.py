3955a708ac23f19190fb91add1d769aba07548a9
LuRy <lury@lury.cz>
2016-01-22 00:51:54 +0100

    update version 1.8

--- chatujmegw.py
+++ chatujmegw.py
@@ -18,7 +18,7 @@ sys.setdefaultencoding('utf8')
 
 PORT = 6667 #Default IRC port
 BIND = "0.0.0.0" #Bind to all IP
-version = 1.7
+version = 1.8
 ua = 'ChatujmeGW/v%s (%s %s) Python %s' %(str(version), sys.platform, os.name, sys.version.split(" ")[0] )
 
 parser = argparse.ArgumentParser(description='ChatujmeGW - v'+str(version))
